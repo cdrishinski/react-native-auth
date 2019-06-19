f078285a983ced1c1169aff1e6571f4fd920f36e
cdrishinski <cdrishinski@gmail.com>
2019-06-18 23:08:34 -0600

    added text input to login form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, TextInput } from 'react-native';
+import { Button, Card, CardSection } from './common';
 
 class LoginForm extends Component {
     render () {
         return (
-            <View />
+            <Card>
+                <CardSection>
+                    <TextInput style={{ height:20, width: 100 }} />
+                    
+                </CardSection>
+                <CardSection>
+
+                </CardSection>
+                <CardSection>
+                    <Button>
+                        Log In
+                    </Button>
+                </CardSection>
+            </Card>
         )
     }
 }
+
+const styles = {
+
+}
+
+export default LoginForm;
