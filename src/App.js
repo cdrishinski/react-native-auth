d5d839d89c561e66ead735c1f7716f59a6a5c4c0
cdrishinski <cdrishinski@gmail.com>
2019-06-19 22:02:01 -0600

    finalized app. renderContent helper funcation created using a switch

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
+import { Header, Button, Spinner } from './components/common';
 import LoginForm from './components/LoginForm'
 
 
 
 class App extends Component {
+    state = { loggedIn: null };
+
     //pulls data from firebase db , componentWillMount does this before render() is run
     componentWillMount() {
         firebase.initializeApp({
@@ -17,14 +19,39 @@ class App extends Component {
                 storageBucket: "authentication-f323f.appspot.com",
                 messagingSenderId: "623457039838",
                 appId: "1:623457039838:web:46e3174c4b0d0ae9"
-        })
+        });
+
+    firebase.auth().onAuthStateChanged((user) => {
+        if (user){
+            this.setState({ loggedIn: true})
+        } else {
+            this.setState({ loggedIn: false })
+        }
+    });
+    }
+
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+                return (
+                    <Button 
+                        onPress={() => firebase.auth().signOut()}>
+                            Log Out
+                    </Button>
+                )
+            case false:
+                return <LoginForm />
+            default:
+                return <Spinner size='large' />
+           }
+
     }
 
     render() {
         return(
             <View>
                 <Header headerText="Authentication" />
-                <LoginForm />
+                {this.renderContent()}
             </View>
         );
         };
